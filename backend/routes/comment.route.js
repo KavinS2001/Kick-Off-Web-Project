2909b3da534b2efbc6b927a41e25a217271a8931
Kavindu <kavindusarathchandraaa@gmail.com>
2025-03-12 20:47:18 +0530

    Comment

--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -3,10 +3,10 @@ const { createComment, getCommentsByPostId } = require("../controllers/comment.c
 
 const router = express.Router();
 
-// Route to create a new comment
+
 router.post("/", createComment);
 
-// Route to get all comments for a specific post
+
 router.get("/:postId", getCommentsByPostId);
 
 module.exports = router;
